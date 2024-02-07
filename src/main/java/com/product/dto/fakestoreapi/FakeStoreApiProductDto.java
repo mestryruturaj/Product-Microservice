33b359dbe34c67e75f14b771cbeb58f8ebb364b5
Ruturaj Mestry <mestryruturaj@gmail.com>
2024-02-07 09:21:03 +0530

    Implementing Response to be received from fakestoreapi.com third party service

---
+++ src/main/java/com/product/dto/fakestoreapi/FakeStoreApiProductDto.java
@@ -0,0 +1,14 @@
+package com.product.dto.fakestoreapi;
+
+import lombok.Data;
+
+@Data
+public class FakeStoreApiProductDto {
+    private Long id;
+    private String title;
+    private Double price;
+    private String description;
+    private String category;
+    private String image;
+    private Rating rating;
+}
---
+++ src/main/java/com/product/dto/fakestoreapi/Rating.java
@@ -0,0 +1,9 @@
+package com.product.dto.fakestoreapi;
+
+import lombok.Data;
+
+@Data
+public class Rating {
+    private Double rate;
+    private Integer count;
+}
