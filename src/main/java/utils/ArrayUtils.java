9942c3a4827c9433b369ea95a2ad8b16b1d36430
Ruturaj Mestry <mestryruturaj@gmail.com>
2024-02-08 08:42:37 +0530

    Added utility class to work on arrays

---
+++ src/main/java/utils/ArrayUtils.java
@@ -0,0 +1,13 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ArrayUtils {
+    public static <T> List<T> convertArrayToList(T[] array) {
+        List<T> ans = new ArrayList<>();
+        Collections.addAll(ans, array);
+        return ans;
+    }
+}
