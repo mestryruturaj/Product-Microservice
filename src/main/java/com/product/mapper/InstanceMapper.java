98bd379b420ad33bcb71d23edfdf5ca8a4180eff
Ruturaj Mestry <mestryruturaj@gmail.com>
2024-02-11 10:43:40 +0530

    Implemented Object conversion class for communication between layers

---
+++ src/main/java/com/product/mapper/InstanceMapper.java
@@ -0,0 +1,65 @@
+package com.product.mapper;
+
+import com.product.dto.controller.ProductRequestDto;
+import com.product.dto.fakestoreapi.FakeStoreApiRequest;
+import com.product.dto.fakestoreapi.FakeStoreApiResponse;
+import com.product.models.Product;
+
+public class InstanceMapper {
+    /**
+     * Method provides an interface for mapping productRequestDto to Product
+     *
+     * @param productRequestDto : ProductResponseDti
+     * @return
+     */
+    public static Product mapProductRequestDtoToProduct(ProductRequestDto productRequestDto) {
+        Product product = new Product();
+        product.setProductName(productRequestDto.getProductName());
+        product.setCategory(productRequestDto.getCategory());
+        product.setDescription(productRequestDto.getDescription());
+        product.setPrice(productRequestDto.getPrice());
+        product.setImageUrl(productRequestDto.getImageUrl());
+        product.setRating(productRequestDto.getRating());
+        return product;
+    }
+
+    /**
+     * Method provides an interface for mapping product to FakeStoreApiRequest
+     *
+     * @param product
+     * @return
+     */
+    public static FakeStoreApiRequest mapProductToFakeStoreApiRequst(Product product) {
+        FakeStoreApiRequest fakeStoreApiRequest = new FakeStoreApiRequest();
+        fakeStoreApiRequest.setTitle(product.getProductName());
+        fakeStoreApiRequest.setCategory(product.getCategory());
+        fakeStoreApiRequest.setDescription(product.getDescription());
+        fakeStoreApiRequest.setImage(product.getImageUrl());
+        fakeStoreApiRequest.setPrice(product.getPrice());
+        return fakeStoreApiRequest;
+    }
+
+    /**
+     * Method provides an interface for mapping fakeStoreApiResponse to Product
+     *
+     * @param fakeStoreApiResponse : FakeStoreApiResponse
+     * @return Product
+     */
+    public static Product mapFakeStoreApiResponseToProduct(FakeStoreApiResponse fakeStoreApiResponse) {
+        Product product = new Product();
+        product.setProductName(fakeStoreApiResponse.getTitle());
+        product.setCategory(fakeStoreApiResponse.getCategory());
+        product.setDescription(fakeStoreApiResponse.getDescription());
+        product.setPrice(fakeStoreApiResponse.getPrice());
+        product.setImageUrl(fakeStoreApiResponse.getImage());
+        product.setRating(fakeStoreApiResponse.getRating());
+        return product;
+    }
+
+
+    /*
+     * TODO:
+     *  1. Product to ProductResponse
+     */
+
+}
