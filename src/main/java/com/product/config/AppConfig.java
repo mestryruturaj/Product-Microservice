fd01fc3f47fc273fa1d8c400899641a0ce4bd36e
Ruturaj Mestry <mestryruturaj@gmail.com>
2024-05-10 08:56:43 +0530

    Development: Configuration and properties

---
+++ src/main/java/com/product/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.product.config;
+
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.metamodel.Metamodel;
+import org.hibernate.engine.spi.SessionFactoryDelegatingImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration
+public class AppConfig {
+}
